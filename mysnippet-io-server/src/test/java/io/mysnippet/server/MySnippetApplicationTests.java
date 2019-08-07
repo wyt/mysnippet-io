233e352aa222e5e0c995975fbf859135f033a4a4
wangyongtao <wangyt4dev@gmail.com>
2019-08-07 19:58:37 +0800

    优化项目结构.

--- mysnippet-io-server/src/test/java/io/mysnippet/server/MySnippetApplicationTests.java
+++ mysnippet-io-server/src/test/java/io/mysnippet/server/MySnippetApplicationTests.java
@@ -10,5 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class MySnippetApplicationTests {
 
   @Test
-  public void contextLoads() {}
+  public void contextLoads() {
+
+  }
 }
