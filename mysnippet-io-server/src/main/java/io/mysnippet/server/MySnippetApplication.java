52bfe270a4aef6f03c22039fdf13e8a2a182ca3a
wangyongtao <wangyt4dev@gmail.com>
2019-08-07 19:41:58 +0800

    调整项目结构.

---
+++ mysnippet-io-server/src/main/java/io/mysnippet/server/MySnippetApplication.java
@@ -0,0 +1,12 @@
+package io.mysnippet.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySnippetApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MySnippetApplication.class, args);
+  }
+}
